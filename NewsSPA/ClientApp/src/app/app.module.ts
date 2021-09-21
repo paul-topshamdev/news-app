63d23469443ffbc6d5db8c8a22ee0fe9ed3ed67e
Paul Marquick <paul.marquick@gmail.com>
2021-09-21 08:19:23 +0100

    Removed fetch data page.

--- NewsSPA/ClientApp/src/app/app.module.ts
+++ NewsSPA/ClientApp/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NewsComponent } from './news/news.component';
 
 @NgModule({
@@ -17,7 +16,6 @@ import { NewsComponent } from './news/news.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
     NewsComponent
   ],
   imports: [
@@ -27,7 +25,6 @@ import { NewsComponent } from './news/news.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'news', component: NewsComponent }
     ])
   ],
